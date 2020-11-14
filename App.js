6a3150f84b2e7e46d608a3eec6bcc5f282ea6b06
martin-505 <martin.gonzalez.2800@gmail.com>
2020-11-14 16:53:13 -0600

    Config deploy

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,TouchableOpacity,ScrollView,SafeAreaView } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
 
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/Register'
 import Navigator from './routes/homeStack';
+
+Amplify.configure(config)
+
+export default withAuthenticator(App)
 
 export default function App() {
   
